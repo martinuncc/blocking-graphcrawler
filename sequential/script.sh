3a791dbae9f8df5c9b146ddfb6cbd4f22d42cbc6
Martin Chisom Anuonye <manuonye@gal-i9.charlotte.edu>
2026-03-03 21:51:18 -0500

    output and script

---
+++ sequential/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=blocking
+#SBATCH --partition=Centaurus
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=10G
+
+make
+./client "Tom Hanks" 1
+./client "Tom Hanks" 2
+./blocking "Tom Hanks" 1
+./blocking "Tom Hanks" 4
